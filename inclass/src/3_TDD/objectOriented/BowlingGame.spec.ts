98e7b1e0267645191fd5166430b9f10a4f30f94d
Jfeerchan23 <56355924+Jfeerchan23@users.noreply.github.com>
2023-09-11 22:49:04 -0600

    ejercicio tdd_oo

--- inclass/src/3_TDD/objectOriented/BowlingGame.spec.ts
+++ inclass/src/3_TDD/objectOriented/BowlingGame.spec.ts
@@ -1,11 +1,61 @@
 import { BowlingGame } from "./BowlingGame";
 
-describe('Test Bowling Game Functionality', () => {
 
-  let game: BowlingGame;
-
+describe('A bowling game', () => {
+    
+  let game:BowlingGame
   beforeEach(() => {
     game = new BowlingGame()
   });
 
+
+  it('can roll a ball', () => {
+    game.roll(0);
+  })
+
+
+  it('should calculate score with all zero', () => {
+    rollMany(game, 20, 0);
+    expect(game.score()).toBe(0)
+  });
+
+  it('should calculate score with all one', () => {
+    rollMany(game, 20, 1);
+
+    expect(game.score()).toBe(20)
+  });
+  it('can roll a spare ', () => {
+    rollSpare(game);
+    game.roll(4)
+    rollMany(game, 17, 0)
+    expect(game.score()).toBe(18)
+   
+  });
+
+  it('can roll a strike', () => {
+    game.roll(10)
+    game.roll(1)
+    game.roll(1)
+    rollMany(game, 16, 0)
+    expect(game.score()).toBe(14)
+  
+  });
+  it('can roll a perfect game', () => {
+    rollMany(game, 12, 10);
+
+    expect( game.score() ).toBe(300);
+  })
+
 });
+function rollMany(game: BowlingGame, rolls: number, pins: number) {
+  for (let i = 0; i < rolls; i++) {
+    game.roll(pins);
+  }
+
+  return game;
+}
+
+function rollSpare(game: BowlingGame) {
+  game.roll(5);
+  game.roll(5);
+}
--- inclass/src/3_TDD/objectOriented/BowlingGame.ts
+++ inclass/src/3_TDD/objectOriented/BowlingGame.ts
@@ -1,6 +1,45 @@
-
 export class BowlingGame {
 
-  constructor() { }
+  private throws: number[];
+
+  constructor() {
+    this.throws = [];
+  }
+
+ public roll(pins: number): void {
+    this.throws.push(pins);
+    
+  }
+
+ public score(): number {
+
+    let sum = 0;
+    let firstRoll = 0;
+
+    for (let i = 0; i < 10; i++) {
+      if (this.isSpare(firstRoll)) {
+        sum += 10 + this.throws[firstRoll + 2];
+        firstRoll += 2;
+      } else if (this.isStrike(firstRoll)) {
+        sum += 10 + this.throws[firstRoll + 1] + this.throws[firstRoll + 2]
+        firstRoll++
+      } else {
+        sum = sum + this.throws[firstRoll] + this.throws[firstRoll + 1];
+        firstRoll = firstRoll + 2;
+      }
+
+    }
+    return sum;
+  }
+
+  private isStrike(index: number) : boolean {
+    return this.throws[index] == 10;
+  }
+
+  private isSpare(index: number):boolean {
+    return (this.throws[index]
+      + this.throws[index + 1]) == 10;
+  }
+
 
 }
